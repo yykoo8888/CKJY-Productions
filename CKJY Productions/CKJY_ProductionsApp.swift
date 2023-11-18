fdfeaa91db2104b075da02470412b32165ae7d02
Yun Ye <yunye.koo10@rafflesgirlssch.edu.sg>
2023-11-18 08:53:53 +0800

    Initial Commit

---
+++ CKJY Productions/CKJY_ProductionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CKJY_ProductionsApp.swift
+//  CKJY Productions
+//
+//  Created by Koo  on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CKJY_ProductionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CKJY ProductionsUITests/CKJY_ProductionsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CKJY_ProductionsUITestsLaunchTests.swift
+//  CKJY ProductionsUITests
+//
+//  Created by Koo  on 18/11/23.
+//
+
+import XCTest
+
+final class CKJY_ProductionsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
